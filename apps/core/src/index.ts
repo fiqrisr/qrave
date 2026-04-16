8405295c022de1536b889975b1c2a6d52cecaa59
Fiqri Syah Redha <wibulo27@gmail.com>
2026-04-16 23:31:39 +0700

    feat(core): init core app

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -1,7 +1,14 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/user/:name", ({ params: { name }, query: { age } }) => name, {
+    query: t.Object({
+      age: t.Number(),
+    }),
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
